96e74de1f0014f41250414e9962db18994abf94c
Ruslan.Kutdusov <ruslan.kutdusov.usu@gmail.com>
2013-05-22 17:58:14 +0600

    commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,115 +8,41 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-
-//    Matrix t = Matrix::TranslateMatrix(1, 2, 3);
-//    t = t*Matrix::RotateX(2);
-//    Matrix tt = t.inverse();
-//    Matrix e = t*tt;
-//    Vector v = Vector(2, -2, 0);
-//    v = t.mul(v);
-//    Vector v1 = tt.mul(v);
     ui->setupUi(this);
-    drawed = false;
-    //XZ1
-    double box_size = 6;
-    Material m3(Color(), Color(0.7, 0.2, 0.2), Color(0.7, 0.2, 0.2), 5, 8, 0, 0);
-    Material m4(Color(), Color(0.2, 0.7, 0.2), Color(0.2, 0.7, 0.2), 5, 8, 0, 0);
-    Material m1(Color(), Color(0.3, 0.6, 0.5), Color(0.3, 0.6, 0.5), 5, 8, 0, 0);
-//    objects.push_back(new ObjectPlane(Vector(-box_size, -box_size,  box_size),
-//                                      Vector( box_size, -box_size,  box_size),
-//                                      Vector(-box_size, -box_size, -box_size),
-//                                      m3));
-//    //XZ2
-//    objects.push_back(new ObjectPlane(Vector( box_size, box_size,  box_size),
-//                                      Vector(-box_size, box_size,  box_size),
-//                                      Vector( box_size, box_size, -box_size),
-//                                      m4));
-//    //YZ_far
-//    objects.push_back(new ObjectPlane(Vector(-box_size, box_size, box_size),
-//                                      Vector(-box_size, -box_size, box_size),
-//                                      Vector(-box_size, box_size, -box_size),
-//                                      m1));
-//    //YZ_near
-//    objects.push_back(new ObjectPlane(Vector(box_size, -box_size, box_size),
-//                                      Vector(box_size, box_size, box_size),
-//                                      Vector(box_size, -box_size, -box_size),
-//                                      m3));
-//    //XY_bottom
-//    objects.push_back(new ObjectPlane(Vector(box_size, box_size,  -5),
-//                                      Vector(-box_size, box_size, -5),
-//                                      Vector(box_size, -box_size, -5),
-//                                      m1));
-//    //XY_top
-//    objects.push_back(new ObjectPlane(Vector(-box_size,  box_size, box_size + EPSILON),
-//                                      Vector( box_size,  box_size, box_size + EPSILON),
-//                                      Vector(-box_size, -box_size, box_size + EPSILON),
-//                                      m1));
+    Material m1(Color(), Color(0.4, 0.1, 0.1), Color(0.5, 0.5, 0.5), 5, 8, 0, 0);
+    Material m2(Color(), Color(0.1, 0.4, 0.1), Color(0.5, 0.5, 0.5), 5, 8, 0, 0);
+    Material m3(Color(), Color(0.1, 0.1, 0.4), Color(0.5, 0.5, 0.5), 5, 8, 0, 0);
+    Material m4(Color(), Color(0.4, 0.4, 0.1), Color(0.5, 0.5, 0.5), 5, 8, 0, 0);
+    Material m5(Color(), Color(0.1, 0.4, 0.4), Color(0.5, 0.5, 0.5), 5, 8, 0, 0);
+    Material m6(Color(), Color(0.2, 0.2, 0.2), Color(0.2, 0.2, 0.2), 0.1, 2, 0, 0);
 
-    //objects.push_back(new ObjectPlane(Vector(0, 0, 3), 12, 12, 0, PI / 2, 0 , m3, true));
-
-
-    Material m5(Color(), Color(0.5, 0.5, 0.7), Color(0.5, 0.5, 0.7), 100, 2, 0, 1);
-    Material m6(Color(), Color(0.5, 0.5, 0.5), Color(0.5, 0.5, 0.5), 0.1, 2, 1, 0.1);
-    //objects.push_back(new ObjectPlane(Vector(0, 0, 0.1), 6, 6, 1.1, 0, 0 , m1));
-//    double z = 0.3;
-//    double y = 0.1;
-//    double x = 0;
-//    objects.push_back(new ObjectPlane(Vector(0, 0, 1.5),  3, 3, x,           y,            z , m1));
-//    objects.push_back(new ObjectPlane(Vector(0, 0, -1.5), 3, 3, x,           y,            z , m1, true));
-//    objects.push_back(new ObjectPlane(Vector(0, 0, 1.5),  3, 3, x,            PI / 2 + y,  z , m1));
-//    objects.push_back(new ObjectPlane(Vector(0, 0, 1.5),  3, 3, x,           -PI / 2 + y,  z , m1));
-//    objects.push_back(new ObjectPlane(Vector(0, 0, 1.5),  3, 3, PI / 2 + x,  y,            z , m1));
-//    objects.push_back(new ObjectPlane(Vector(0, 0, 1.5),  3, 3, -PI / 2 + x, y,            z , m1));
-
-    Matrix m = Matrix::TranslateMatrix(-4, 0, 0);
+    double box_size = 12;
+    //YZ far
+    Matrix m = Matrix::TranslateMatrix(-box_size / 2, 0, 0);
     m = Matrix::RotateY(PI / 2) * m;
-    objects.push_back(new ObjectPlane(m, 15, 15, m3));
+    objects.push_back(new ObjectPlane(m, box_size, box_size, m1));
+    //XY top
+    m = Matrix::TranslateMatrix(0, 0, box_size / 2);
+    objects.push_back(new ObjectPlane(m, box_size, box_size, m2, true));
+    //XY bottom
+    m = Matrix::TranslateMatrix(0, 0, -box_size / 2);
+    objects.push_back(new ObjectPlane(m, box_size, box_size, m3));
+    //XZ left
+    m = Matrix::TranslateMatrix(0, box_size / 2, 0);
+    m = Matrix::RotateX(PI / 2) * m;
+    objects.push_back(new ObjectPlane(m, box_size, box_size, m4));
+    //XZ right
+    m = Matrix::TranslateMatrix(0, -box_size / 2, 0);
+    m = Matrix::RotateX(-PI / 2) * m;
+    objects.push_back(new ObjectPlane(m, box_size, box_size, m5));
 
-//    double x = 0.8;
-//    double y = 0.2;
-//    double z = 0.4;
-//    Vector pos(0, 0, -1);
-//    m = Matrix::TranslateMatrix(0, 0, -1.5);
-//    m = m * Matrix::RotateX(x) * Matrix::RotateY(y) * Matrix::RotateZ(z) * Matrix::TranslateMatrix(pos);
-//    objects.push_back(new ObjectPlane(m, 3, 3, m1));
+    objects.push_back(new ObjectBox(Vector(0, -2, -box_size / 2 + 1.5), Vector(0, 0, -0.5), 3, m6));
+    //objects.push_back(new ObjectBox(Vector(0, 0, 0), Vector(0, 0, 0.4), 3, m6));
 
-//    m = Matrix::TranslateMatrix(0, 0, 1.5);
-//    m = m * Matrix::RotateX(x) * Matrix::RotateY(y) * Matrix::RotateZ(z) * Matrix::TranslateMatrix(pos);
-//    objects.push_back(new ObjectPlane(m, 3, 3, m1));
+    objects.push_back(new ObjectSphere(Vector(1, 2, -box_size / 2 + 1.5), 1.5, m1));
 
-//    m = Matrix::TranslateMatrix(-1.5, 0, 0);
-//    m = Matrix::RotateY(-PI / 2) * m;
-//    m = m * Matrix::RotateX(x) * Matrix::RotateY(y) * Matrix::RotateZ(z) * Matrix::TranslateMatrix(pos);
-//    objects.push_back(new ObjectPlane(m, 3, 3, m1));
-
-//    m = Matrix::TranslateMatrix(1.5, 0, 0);
-//    m = Matrix::RotateY(PI / 2) * m;
-//    m = m * Matrix::RotateX(x) * Matrix::RotateY(y) * Matrix::RotateZ(z) * Matrix::TranslateMatrix(pos);
-//    objects.push_back(new ObjectPlane(m, 3, 3, m1));
-
-//    m = Matrix::TranslateMatrix(0, 1.5, 0);
-//    m = Matrix::RotateX(-PI / 2) * m;
-//    m = m * Matrix::RotateX(x) * Matrix::RotateY(y) * Matrix::RotateZ(z) * Matrix::TranslateMatrix(pos);
-//    objects.push_back(new ObjectPlane(m, 3, 3, m1));
-
-//    m = Matrix::TranslateMatrix(0, -1.5, 0);
-//    m = Matrix::RotateX(PI / 2) * m;
-//    m = m * Matrix::RotateX(x) * Matrix::RotateY(y) * Matrix::RotateZ(z) * Matrix::TranslateMatrix(pos);
-//    objects.push_back(new ObjectPlane(m, 3, 3, m1));
-
-    objects.push_back(new ObjectBox(Vector(0, 0, -3), Vector(0, 0, 1), 3, m1));
-
-//    objects.push_back(new ObjectSphere(Vector(0, 0, 0), 3, m6));
-    //objects.push_back(new ObjectPlane(Vector(0, 0, -2), 2, 2, 0, 0, 1, m1));
-
-//    objects.push_back(new ObjectPlane(Vector(-3, 0, 0),
-//                                      Vector(-3, 3, 0),
-//                                      Vector(-3, 0, -3),
-//                                      m6));
-
-    //lights.push_back(ObjectLight(Vector(5, 5, 5), Color(0.7, 0.7, 0.8)));
-    lights.push_back(ObjectLight(Vector(4, 0, 3), Color(0.8, 0.6, 0.7)));
+    lights.push_back(ObjectLight(Vector(4, -4, 4), Color(0.7, 0.7, 0.8)));
+    lights.push_back(ObjectLight(Vector(4, 4, 4), Color(0.8, 0.6, 0.7)));
 
     //lights.push_back(ObjectLight(Vector(-4, 4, 4), light_material));
 //    Material light_material2(QColor(100, 0,0 ), QColor(0, 0, 0), QColor(0, 0, 0));
@@ -193,8 +119,8 @@ Color MainWindow::ray_tracing(const Ray & ray, const int &depth, int & rays_coun
     Color reflect_ray_color;
     //if (objects[i_object]->m_material.m_refract_amount < 0.8)
     {
-//        reflect_ray_color = ray_tracing(intr.reflect_ray, depth_, rays_count, &d);
-//        reflect_ray_color = reflect_ray_color * exp(-objects[i_object]->m_material.m_beta);
+        reflect_ray_color = ray_tracing(intr.reflect_ray, depth_, rays_count, &d);
+        reflect_ray_color = reflect_ray_color * exp(-objects[i_object]->m_material.m_beta);
     }
 
     Color refract_ray_color;
@@ -218,13 +144,14 @@ void MainWindow::paintEvent(QPaintEvent *pevent){
 //    painter.translate(pevent->rect().width() / 2, pevent->rect().height() / 2);
 //    painter.drawPoint(0, 0);
     double ratio = (double)pevent->rect().width() / (double)pevent->rect().height();
-    Vector camera_pos(5.8, 0, 0);
+    Vector camera_pos(17, 0, 0);
     double w = 6;
     double h = w / ratio;
-    Viewport viewport(Vector(4, -w/2, h/2),
-                      Vector(4, w/2, h/2),
-                      Vector(4, -w/2, -h/2),
-                      Vector(4, w/2, -h/2));
+    double f = 12;
+    Viewport viewport(Vector(f, -w/2, h/2),
+                      Vector(f, w/2, h/2),
+                      Vector(f, -w/2, -h/2),
+                      Vector(f, w/2, -h/2));
     double step_y = w / pevent->rect().width();
 
     for(double y = viewport.m_p1.y, xx = 0; y < viewport.m_p2.y; y += step_y, xx++){
@@ -232,6 +159,9 @@ void MainWindow::paintEvent(QPaintEvent *pevent){
         for(double z = viewport.m_p1.z, yy = 0; z > viewport.m_p3.z; z -= step_y, yy++){
             Vector viewport_point(viewport.m_p1.x, y, z);
             Ray first_ray(viewport_point, camera_pos);
+            Matrix m = Matrix::RotateY(-0.2);
+            first_ray.start_point = camera_pos + Vector(0, 0, 2);
+            first_ray.vector = m.mul(first_ray.vector);
             Color c =ray_tracing(first_ray, 0, rays_count, NULL);
             QPen p = QPen(c.convert());
             painter.setPen(p);
